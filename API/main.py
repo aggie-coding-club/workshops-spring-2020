2c812190dea58a760b307db4cab24392e35de107
shreyshah33 <shreyshah33@gmail.com>
2020-02-13 19:11:23 -0600

    Made changes to API code. Added Comments

--- API/main.py
+++ API/main.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    # GET
     if request.method == 'GET':
+
+        # if get return a form
         return """
         <html>
             <form method = post enctype = multipart/form-data>
@@ -17,21 +20,31 @@ def home():
             </form>
         </html>
         """
+
+   # POST
     else:
+        # get form query
         query = request.form['query']
+
+        # get links from the user generated query
         resp = requests.get(
-            "https://api.stackexchange.com/2.2/similar?order=desc&sort=relevance&title="+query+"&site=stackoverflow")
+            "https://api.stackexchange.com/2.2/similar?order=desc&sort=relevance&title=" + query + "&site=stackoverflow")
+
+        # get items form the API response
         items = resp.json()["items"]
-        links = ""
+
+        # get the first answered link
+        link = ""
         for item in items:
             if item["is_answered"]:
-                links = item["link"]
+                link = item["link"]
                 break
-        print(links)
-        if links == "":
+
+        # if no answers return No Answer Exists
+        if link == "":
             return """
             <html>
-                <h1>No Webpage exists</h1>
+                <h1>No Answers exists</h1>
                 <form method = post enctype = multipart/form-data>
                     <label> Enter query </label > <input type = "text" name = "query">
                     <br>
@@ -39,7 +52,9 @@ def home():
                 </form>
             </html>
             """
-        return redirect(links)
+
+        # If link exists, redirect to it
+        return redirect(link)
 
 
 if __name__ == '__main__':
